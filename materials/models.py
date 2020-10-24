288b1afd8e1abecb36ec7abb4cb6f61a4174c27a
Chethan Jagannatha Kulkarni <chethanjkulkarni@gmail.com>
2020-10-24 12:34:17 +0530

    Added excel sheets

--- materials/models.py
+++ materials/models.py
@@ -1,9 +1,53 @@
+from accounts.models import User
+from core.models import Mill
 from django.db import models
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=64)
-    
+    is_deleted = models.BooleanField(default=False)
+    mill = models.ForeignKey(to=Mill, on_delete=models.PROTECT)
+    created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now=True)
 
-class Source(models.Model):
+class IncomingSource(models.Model):
+    name = models.CharField(max_length=64)
+    is_deleted = models.BooleanField(default=False)
+    mill = models.ForeignKey(to=Mill, on_delete=models.PROTECT)
+    created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class OutgoingSource(models.Model):
+    name = models.CharField(max_length=64)
+    is_deleted = models.BooleanField(default=False)
+    mill = models.ForeignKey(to=Mill, on_delete=models.PROTECT)
+    created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class ProcessingSide(models.Model):
+    name = models.CharField(max_length=64)
+    is_deleted = models.BooleanField(default=False)
+    mill = models.ForeignKey(to=Mill, on_delete=models.PROTECT)
+    created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class Stock(models.Model):
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='stocks')
+    stock = models.FloatField(default=0.0)
+    is_deleted = models.BooleanField(default=False)
+    mill = models.ForeignKey(to=Mill, on_delete=models.PROTECT)
+    created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now=True)
+
+
+class IncomingStockEntry(models.Model):
+    pass
+
+class OutgoingStockEntry(models.Model):
+    pass
+
+class ProcessingStockEntry(models.Model):
     pass
