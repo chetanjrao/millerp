0878179dfe0e569f8cdb4b84e68a2d8b78cc7c6f
Chethan Jagannatha Kulkarni <chethanjkulkarni@gmail.com>
2020-10-25 18:20:01 +0530

    Added fix

--- core/models.py
+++ core/models.py
@@ -6,5 +6,7 @@ class Mill(models.Model):
     name = models.CharField(max_length=128)
     address = models.TextField()
     access = models.ManyToManyField(to=User)
-    is_active = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str: return self.name
