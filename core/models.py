34b9c0e03a68674442586eeecc06bead10cd6f9a
Chethan Jagannatha Kulkarni <chethanjkulkarni@gmail.com>
2020-10-25 13:11:52 +0530

    Added materials models

--- core/models.py
+++ core/models.py
@@ -1,6 +1,10 @@
+from accounts.models import User
 from django.db import models
 
 # Create your models here.
 class Mill(models.Model):
     name = models.CharField(max_length=128)
     address = models.TextField()
+    access = models.ManyToManyField(to=User)
+    is_active = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now=True)
--- materials/models.py
+++ materials/models.py
@@ -2,7 +2,6 @@ from accounts.models import User
 from core.models import Mill
 from django.db import models
 
-# Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=64)
     is_deleted = models.BooleanField(default=False)
@@ -17,37 +16,47 @@ class IncomingSource(models.Model):
     created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now=True)
 
-
 class OutgoingSource(models.Model):
     name = models.CharField(max_length=64)
-    is_deleted = models.BooleanField(default=False)
     mill = models.ForeignKey(to=Mill, on_delete=models.PROTECT)
+    is_deleted = models.BooleanField(default=False)
     created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now=True)
-
 
 class ProcessingSide(models.Model):
     name = models.CharField(max_length=64)
-    is_deleted = models.BooleanField(default=False)
     mill = models.ForeignKey(to=Mill, on_delete=models.PROTECT)
+    is_deleted = models.BooleanField(default=False)
     created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
     created_at = models.DateTimeField(auto_now=True)
-
-
-class Stock(models.Model):
-    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='stocks')
-    stock = models.FloatField(default=0.0)
-    is_deleted = models.BooleanField(default=False)
-    mill = models.ForeignKey(to=Mill, on_delete=models.PROTECT)
-    created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
-    created_at = models.DateTimeField(auto_now=True)
-
 
 class IncomingStockEntry(models.Model):
-    pass
+    date = models.DateField(auto_now=True)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='incomingstockentrys')
+    source = models.ForeignKey(to=IncomingSource, on_delete=models.CASCADE, related_name='incomingstockentrys')
+    bags = models.FloatField()
+    average_weight = models.FloatField()
+    is_deleted = models.BooleanField(default=False)
+    created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now=True)
 
 class OutgoingStockEntry(models.Model):
-    pass
+    date = models.DateField(auto_now=True)
+    stock = models.ForeignKey(to=IncomingStockEntry, on_delete=models.PROTECT)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='outgoingstockentrys')
+    source = models.ForeignKey(to=OutgoingSource, on_delete=models.CASCADE, related_name='outgoingstockentrys')
+    bags = models.FloatField()
+    average_weight = models.FloatField()
+    is_deleted = models.BooleanField(default=False)
+    created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now=True)
 
 class ProcessingStockEntry(models.Model):
-    pass
+    stock = models.ForeignKey(to=IncomingStockEntry, on_delete=models.PROTECT)
+    category = models.ForeignKey(to=Category, on_delete=models.CASCADE, related_name='outgoingstockentrys')
+    source = models.ForeignKey(to=OutgoingSource, on_delete=models.CASCADE, related_name='outgoingstockentrys')
+    bags = models.FloatField()
+    average_weight = models.FloatField()
+    is_deleted = models.BooleanField(default=False)
+    created_by = models.ForeignKey(to=User, on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now=True)
