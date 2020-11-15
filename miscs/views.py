b6f893767f0f0f91c858c0b211414ec204b0f24f
Chethan Jagannatha Kulkarni <chethanjkulkarni@gmail.com>
2020-11-15 16:42:23 +0530

    Update cache view

--- miscs/views.py
+++ miscs/views.py
@@ -22,6 +22,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from bs4 import BeautifulSoup, Tag
 from django.views.decorators.cache import cache_page
+from django.core.cache import cache
 from asgiref.sync import sync_to_async
 
 def get_random_string(length):
@@ -147,10 +148,9 @@ def get_guarantee(request: WSGIRequest):
     options.binary_location = CHROME
     driver = webdriver.Chrome(CHROMEDRIVER, options=options)
     firm = Firm.objects.get(pk=request.COOKIES["MERP_FIRM"], is_deleted=False, mill=request.mill)
+    cached_response = cache.get("{}".format(firm.pk))
     data = get_captcha(driver, '{}.png'.format(get_random_string(8)), '{}.png'.format(get_random_string(8)), '{}'.format(firm.username), '{}'.format(firm.password), request.user.mobile)
-    data["firm"] = firm.pk
-    data["username"] = firm.username
-    data["password"] = firm.password
+    cache.set("{}".format(firm.username), data, 60 * 45)
     return JsonResponse(data)
 
 @login_required
