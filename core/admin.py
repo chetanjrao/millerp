444c61ba92d5f725acd376758c46679a89fb4e79
Chethan Jagannatha Kulkarni <chethanjkulkarni@gmail.com>
2020-11-18 01:23:15 +0530

    Added more package info

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
-from core.models import Mill
+from core.models import Mill, Purchase
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(Mill)
+admin.site.register(Purchase)
