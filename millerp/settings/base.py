bba7f57b4ce05d65c3482f56469022b9233ed2f6
Chethan Jagannatha Kulkarni <chethanjkulkarni@gmail.com>
2020-11-27 22:42:52 +0530

    Added flexible payment settins

--- millerp/settings/base.py
+++ millerp/settings/base.py
@@ -25,6 +25,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', True)
+PRODUCTION = env.bool('DEBUG', True)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", [])
 # ALLOWED_HOSTS = ['127.0.0.1']
@@ -134,7 +135,7 @@ WIDTH_RATIO = env.int('WIDTH_RATIO')
 RZP_KEY = env.str('RZP_KEY')
 RZP_SECRET = env.str('RZP_SECRET')
 
-if DEBUG:
+if not PRODUCTION:
     RZP_KEY = env.str('RZP_DEV_KEY')
     RZP_SECRET = env.str('RZP_DEV_SECRET')
 
